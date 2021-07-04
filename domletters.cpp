23342f3329d29281fd478466c1e84794db09937d
Kirani Anderson <kirani@ada.cs.pdx.edu>
2021-07-04 13:23:59 -0700

    testing again

--- domletters.cpp
+++ domletters.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <cctype>
 
 using namespace std;
 
@@ -8,25 +9,40 @@ int charSize;
 
 void getString();
 void deleteChars();
+void increaseCharsCapacity();
+int checkWord(int start, int end);
 
 int main()
 {
 	charSize = 100;
 	chars = new char[charSize];
-	//int start = 0;
-	//bool validStart = true;
+	int start = 0;
+	bool validStart = true;
+	int total = 0;
 
-	getString();
+	getString(); //gets the string from the file
 
-	for (int i = 0; i < charSize; i++)
-		cout << chars[i];
-	//for (int i = start; i < 
+	for (int i = start; i < charSize; i++) //finds words
+	{
+		if (isspace(chars[i]) || i >= charSize - 1) //checks contents of words
+		{
+			if (validStart && i > start+1)
+			{
+				total += checkWord(start, i);
+				start = i;
+			}
+			else
+			{
+				start = i;
+				validStart = true;
+			}
+		}
+		else if (!isalpha(chars[i]))
+			validStart = false;
+	} 
 
 
-
-
-
-
+	cout << total;
 	deleteChars();	
 	return 0;
 }
@@ -38,13 +54,17 @@ void getString() //gets every character including newlines
 	{
 		cin.get(ch);
 		if (cin)
+		{
+			if (i >= charSize)
+				increaseCharsCapacity();
 			chars[i] = ch;	
+		}
 	}
 	cin.clear();
 	cin.ignore(100, '\n');
 }
 
-void increaseCharsCapacity()
+void increaseCharsCapacity() //increases the capacity of the dynamic array 
 {
 	char *chars2 = new char[charSize+100];
 	for (int i = 0; i < charSize; i++)
@@ -54,18 +74,34 @@ void increaseCharsCapacity()
 	charSize += 100;
 }
 
-void deleteChars()
+void deleteChars() //safely clears dynamic memory
 {
 	delete [] chars;
 	chars = nullptr;
 }
 
-int checkWord(int start, int end)
+int checkWord(int start, int end) //counts each appearance of each letter and returns the highest count
 {
+	int * counts = new int[end-start];
+	for (int i = 0; i < end-start; i++)
+		counts[i] = 1;
+	int most;
+	for (int i = start; i <= end; i++) //counts each letter
+	{
+		for (int j = i+1; j <= end; j++)
+		{
+			if (toupper(chars[i]) == toupper(chars[j]))
+				counts[i-start]++;
 
-
-
-	return 1;
+		}
+	}
+	most = 1;
+	for (int i = 1; i < end-start; i++) //finds the highest count
+	{	
+		if (counts[i] > most)
+			most = counts[i];
+	}
+	return most;
 }
 
 
